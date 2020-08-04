dfc2fb32357f36add21288ab4332b1087b483ea2
Doug Dyson <dougdyson@gmail.com>
2020-08-04 04:21:40 +0000

    Reordered /url routes in code so that they are altogether for easier reference and maintenance

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -17,8 +20,13 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls.json", (req, res) => {
