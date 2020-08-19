1a4bc7c334da800b17965ed335513d29daef82d7
Doug Dyson <dougdyson@gmail.com>
2020-08-19 05:04:58 +0000

    registration and not logged in updates

--- express_server.js
+++ express_server.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+const bcrypt = require('bcrypt');
+const cookieSession = require('cookie-session');
 
 app.set("view engine", "ejs");
-
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1','key2'],
 
-cookieParser = require('cookie-parser')
-app.use(cookieParser())
-
-const bcrypt = require('bcrypt');
-
-const charArray = ['a', 'A', 'b', 'C', 'd', 'e', 'f', 'g', 'G', 'h', 'I', 'j', 'k', 'K', 'L', 'm', 'n', 'o', 'p', 'Q', 'r', 'R', 'S', 's', 't', 'u', 'v', 'Y', 'z',' Z', '1', '2', '3', '4', '5', '6', '7', '8', '9','10'];
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
 
 let gUsers = {};
+let gURLDatabase = {};
 
+// seeds for generateRandomString and userID in addNewUser 
+const charArray = ['a', 'A', 'b', 'C', 'd', 'e', 'f', 'g', 'G', 'h', 'I', 'j', 'k', 'K', 'L', 'm', 'n', 'o', 'p', 'Q', 'r', 'R', 'S', 's', 't', 'u', 'v', 'Y', 'z',' Z', '1', '2', '3', '4', '5', '6', '7', '8', '9','10'];
 
+// should move these functions out to a helper file
 function generateRandomString(length, charArray){
   // currently no check for uniqueness!!!
   let randomString = '';
@@ -25,7 +30,6 @@ function generateRandomString(length, charArray){
   }
   return randomString;
 }
-
 function addNewUser (email, password) {
   const userID = generateRandomString(6, charArray)
   const hashedPassword = bcrypt.hashSync(password, 10);
@@ -40,42 +44,44 @@ function addNewUser (email, password) {
 
   return newUser;
 };
-
 function emailExists(email){
 
-  console.log('SOF emailExists:', email);
+  // console.log('SOF emailExists:', email);
 
   for (const key in gUsers) {
     if (gUsers[key].email === email) {
-      console.log('emailExists gUsers[key].email:', gUsers[key].email, 'email:', email);
+      // console.log('emailExists gUsers[key].email:', gUsers[key].email, 'email:', email);
       return true;
     }
   }
   return false;
 }
-
-function getUser(email){
+function getUserByEmail(email){
   let user = {};
+  let database = {}
 
   for (const key in gUsers) {
     if (gUsers[key].email === email) {
       user = gUsers[key];
-      console.log('getUser:', user);
+      // console.log('getUserByEmail:', user);
     }
   }
   return user;
 }
-
-function loginUser (email, password) {
-  const hashedPassword = bcrypt.hashSync(password, 10);
+function checkIfUserExists (email, password) {
+  //const hashedPassword = bcrypt.hashSync(password, 10);
   if (emailExists(email)){
+    user = getUserByEmail(email);
+    const hashedPassword = user.password;
+    // console.log('checkIfUserExists emailExists: true');
     if (bcrypt.compareSync(password, hashedPassword)) {
-      console.log('loginUser: true');
+      // console.log('checkIfUserExists password compare: true');
       return true;
     }
+    // console.log('checkIfUserExists password compare: false');
   }
+  return false;
 }
-
 function emailExists(email){
 
   for (const key in gUsers) {
@@ -87,50 +93,49 @@ function emailExists(email){
 }
 
 
-const urlDatabase = {};
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
 
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
 });
 
-app.get("/hello", (req, res) => {
-  let templateVars = { greeting: 'Hello World!' };
-  res.render("hello_world", templateVars);
-});
+// app.get("/", (req, res) => {
+//   const templateVars = { user: gUsers[req.session.userID]};
+//   if (templateVars) {
+//     res.redirect("/urls");
+//   } else {
+//     res.redirect("/login");
+//   }
+// });
 
 app.get("/register", (req, res) => {
-  let templateVars = { user: gUsers[req.cookies.userID]};
+  let templateVars = { user: gUsers[req.session.userID]};
   res.render("register", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  let templateVars = { user: gUsers[req.cookies.userID] };
+  let templateVars = { user: gUsers[req.session.userID] };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+  res.json(gURLDatabase);
 });
 
 app.get("/urls", (req, res) => {
-  let user = gUsers[req.cookies.userID]
-  let templateVars = { urls: urlDatabase, user: user};
-  console.log("/urls req.cookies.id", req.cookies.userID);
-  console.log("/urls templateVars:", templateVars);
+  let user = gUsers[req.session.userID]
+  let templateVars = { urls: gURLDatabase, user: user};
+  console.log("/urls req.session.id:", req.session.userID, 'templateVars:', templateVars);
+  // console.log("/urls templateVars:", templateVars);
   if (user) {
     res.render("urls_index", templateVars);
   } else {
-    res.render("error");
+    res.render("/login", templateVars);
   }
 });
 
 app.get("/login", (req, res) => {
-  let templateVars = { urls: urlDatabase, user: gUsers[req.cookies.userID]};
-  console.log("/login templateVars:", templateVars);
+  let templateVars = { urls: gURLDatabase, user: gUsers[req.session.userID]};
+  // console.log("/login templateVars:", templateVars);
   res.render("login", templateVars);
 });
 
@@ -147,35 +152,38 @@ app.post("/register", (req, res) => {
 
   let user = addNewUser(req.body.email, req.body.password)
   
-  res.cookie('userID', user.id);
+  //res.cookie('userID', user.id);
+  req.session.userID = user.id;
   res.redirect("/urls"); // need to make this the userID.
 });
 
 app.post("/urls", (req, res) => {
-  console.log('/urls req.body', req.body);  // Log the POST request body to the // console
+  // console.log('/urls req.body', req.body);  // Log the POST request body to the // console
   const urlString = generateRandomString(6, charArray);
-  const userid = req.cookies.userID;
-  console.log('/urls userid', userid);
+  const userid = req.session.userID;
+  // console.log('/urls userid', userid);
   longurl = req.body.longURL
-  urlDatabase[urlString] = {longURL: longurl, userID: gUsers[userid].id};
-  console.log('userid:', userid,'urlDatabase[urlString]', urlDatabase[urlString]);
-  //if (userid) {
+  gURLDatabase[urlString] = {longURL: longurl, userID: gUsers[userid].id};
+  console.log('userid:', userid,'gURLDatabase[urlString]', gURLDatabase[urlString]);
+  if (userid) {
     res.redirect("/urls");// + urlString);
-  //} 
-  // else {
-  //   res.redirect("/error");
-  // }
+  } 
+  else {
+    res.redirect("/error");
+  }
 });
 
 app.post("/login", (req,res) => {
   const email = req.body.email;
   const password = req.body.password;
   
-  console.log('SOF /login email:', email, 'password:', password);
+  // console.log('SOF /login email:', email, 'password:', password);
 
-  if (loginUser (email, password)) {
-    user = getUser(email);x
-    res.cookie('userID', user.id);
+  if (checkIfUserExists (email, password)) {
+    // console.log('/login checkIfUserExists = true');
+    user = getUserByEmail(email);
+    //res.cookie('userID', user.id);
+    req.session.userID = user.id;
     res.redirect("/urls");
   } else {
     throw 400;
@@ -183,29 +191,30 @@ app.post("/login", (req,res) => {
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('userID');
+  req.session = null;
+  // console.log('/logout');
   res.redirect("/login");
 });
 
 app.post("/urls/:shortURL", (req, res) => {
   // // console.log('Update! ' + req.params.longURL);
-  urlDatabase[req.params.shortURL] = req.body.longURL;
+  gURLDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   // // console.log('Delete! ' + req.params.shortURL);
-  delete urlDatabase[req.params.shortURL];
+  delete gURLDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: gUsers[req.cookies.userID] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: gURLDatabase[req.params.shortURL], user: gUsers[req.session.userID] };
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = gURLDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
