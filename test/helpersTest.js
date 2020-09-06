dab012f13bd673a073a44533c525735a98434c80
Doug Dyson <dougdyson@gmail.com>
2020-09-06 08:04:24 +0000

    updated test scripts

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -17,28 +17,45 @@ const testUsers = {
 
 const {getUserByEmail} = helpers(testUsers);
 describe('getUserByEmail', function() {
-  it('should not return not a user with an invalid email', function() {
+  it('should return a user with an valid email', function() {
     const user = getUserByEmail("user@example.com")
     const expectedOutput = "userRandomID";
-    assert.notEqual(user, expectedOutput, true);
+    assert.strictEqual(user.id, expectedOutput, true);
+  });
+
+  it('should return {} for an invalid email', function() {
+    const user = getUserByEmail("admin@example.com")
+    const expectedOutput = undefined;
+    assert.strictEqual(user.id, expectedOutput, true);
   });
 });
 
 const {checkEmailExists} = helpers(testUsers);
 describe('checkEmailExists', function() {
-  it('should return false if a user has an invalid email', function() {
+  it('should return true if a user has an valid email', function() {
     const email = checkEmailExists("user@example.com")
+    const expectedOutput = true;
+    assert.strictEqual(email, expectedOutput, true);
+  });
+
+  it('should return false an invalid email', function() {
+    const email = checkEmailExists("admin@example.com")
     const expectedOutput = false;
-    assert.notEqual(email, expectedOutput, true);
+    assert.strictEqual(email, expectedOutput, true);
   });
 });
 
 const {checkIfUserExists} = helpers(testUsers);
 describe('checkIfUserExists', function() {
-  it('should return false if a user does not exist', function() {
+  it('should return true if a user exists', function() {
     const userExists = checkIfUserExists("user@example.com", "purple-monkey-dinosaur");
-    console.log(userExists);
-    const expectedOutput = false;
+    const expectedOutput = true;
     assert.notEqual(userExists, expectedOutput, true);
+  });
+
+  it('should return false if user does not exist', function() {
+    const userExists = checkIfUserExists("admin@example.com", "purple-monkey-dinosaur");
+    const expectedOutput = false;
+    assert.strictEqual(userExists, expectedOutput, true);
   });
 });
